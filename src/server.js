7f71622a834c540a0354d0af2fbfde0574768578
KenRogers-xpro <kenxpro0@gmail.com>
2026-07-13 12:49:07 +0300

    feat: initialize backend server entry point

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.use(compression())
 app.use(morgan('dev'))
 
 app.use(cors({
-	origin: [process.env.FRONTEND_URL, 'https://uacc-dimsfrontend.vercel.app'],
+	origin: [process.env.FRONTEND_URL, /\.vercel\.app$/],
 	credentials: true,
 	methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization'],
